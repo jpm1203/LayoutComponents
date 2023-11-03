e1e77db9b705a4289489c2c00d6613a1559bbcfc
Jpdm1203 <119541106+Jpdm1203@users.noreply.github.com>
2023-11-02 23:09:51 -0600

    Check

---
+++ ToDoList.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { StyleSheet, Pressable, View, Text, ScrollView, StatusBar } from 'react-native';
+
+export default function ToDoList(){
+    return (
+        <SafeAreaView>
+            <ScrollView>
+                <Pressable>
+                    <View style={[styles.task, styles.completed]}>
+                        <Text style={styles.taskText}>Do laundry</Text>
+                    </View>
+                </Pressable>
+                <Pressable>
+                    <View style={[styles.task]}>
+                        <Text style={styles.taskText}>Go to gym</Text>
+                    </View>
+                </Pressable>
+                <Pressable>
+                    <View style={[styles.task, styles.completed]}>
+                        <Text style={styles.taskText}>Walk dog</Text>
+                    </View>
+                </Pressable>
+            </ScrollView>
+            <StatusBar/>
+        </SafeAreaView>
+    );
+};
+
+const styles = StyleSheet.create({
+  task: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderColor: '#ccc',
+  },
+  completed: {
+    backgroundColor: '#e0e0e0',
+  },
+  taskText: {
+    fontSize: 16,
+  },
+});
